9abfe64f027afeb57cfa9caaaf7526e48b961299
alekelbar <alekelbar.personal@gmail.com>
2023-06-08 16:54:18 -0600

    Add project files.

---
+++ proyecto2/racional.cpp
@@ -0,0 +1,137 @@
+#include "racional.hpp"
+
+int getMCD(int numero1, int numero2)
+{
+	// Asegurarse de que ambos números sean positivos
+	numero1 = abs(numero1);
+	numero2 = abs(numero2);
+
+	// Realizar el algoritmo de Euclides de forma iterativa
+	while (numero2 != 0) {
+		int temp = numero2;
+		numero2 = numero1 % numero2;
+		numero1 = temp;
+	}
+	// El MCD se encuentra en numero1
+	return numero1;
+}
+
+Racional Racional::simplificar()
+{
+	int mcd = getMCD(this->n, this->m);
+	return Racional(this->n / mcd, this->m / mcd);
+	// Pueden consultar el porque aquí: https://www.youtube.com/watch?v=0opNDyuqCfg
+}
+
+Racional::Racional(int dividendo, int divisor) : n(dividendo), m(divisor) {}
+
+Racional::~Racional(){}
+
+Racional Racional::operator=(Racional racional)
+{
+	return Racional(racional.n, racional.m);
+}
+
+
+// UNARIOS...
+Racional Racional::operator+() {
+	// ¿Que cambia? 
+}
+
+Racional Racional::operator-() {
+	// ¿Que cambia? 
+	this->n * -1, this->m * -1;
+}
+
+// BINARIOS...
+Racional Racional::operator+(Racional racional) {
+	// ¿Como se suman fracciones?
+	// a   c   ad + bc
+	// b + d =   bd
+	int mul_num = this->n * racional.den();
+	int mul_den = this->m * racional.num();
+	return Racional(mul_num + mul_den, this->m * racional.den());
+}
+
+Racional Racional::operator-(Racional racional) {
+	// ¿Como se restan fracciones?
+	// a   c   ad - bc
+	// b - d =   bd
+	int mul_num = this->n * racional.den();
+	int mul_den = this->m * racional.num();
+	return Racional(mul_num - mul_den, this->m * racional.den());
+}
+
+Racional Racional::operator*(Racional racional)
+{
+	// ¿Como se multiplican fracciones?
+	// a   c     ac
+	// b * d =   bd
+	return Racional(this->n * racional.num(), this->m * racional.den());
+}
+
+Racional Racional::operator/(Racional racional)
+{
+	// ¿Como se dividen fracciones?
+	// a/b
+	// c/d = a*d/b*c
+	return Racional(this->n * racional.den(), this->m * racional.num());
+}
+
+void Racional::operator-=(Racional racional)
+{
+	*this = *this - racional;
+}
+
+void Racional::operator+=(Racional racional)
+{
+	*this = *this + racional;
+}
+
+void Racional::operator*=(Racional racional)
+{
+	*this = *this * racional;
+}
+
+void Racional::operator/=(Racional racional)
+{
+	*this = *this / racional;
+}
+
+// Porque las comparaciones las hago así: https://www.youtube.com/watch?v=u1XSGdyX4XQ y tambien
+// https://www.youtube.com/watch?v=DkIeIJFpJUY
+
+bool Racional::operator==(Racional racional)
+{
+	Racional simplifyRational = this->simplificar();
+	racional = racional.simplificar();
+
+	return (simplifyRational.num() == racional.n) && (simplifyRational.den() == racional.m);
+}
+
+bool Racional::operator!=(Racional racional)
+{
+	racional = racional.simplificar();
+	return !(*this == racional);
+}
+
+bool Racional::operator>(Racional racional)
+{
+	return (this->n * racional.m) > (this->m * racional.n);
+}
+
+bool Racional::operator>=(Racional racional)
+{
+	return (this->n * racional.m) >= (this->m * racional.n);
+}
+
+bool Racional::operator<(Racional racional)
+{
+	return (this->n * racional.m) < (this->m * racional.n);
+}
+
+bool Racional::operator<=(Racional racional)
+{
+	return (this->n * racional.m) <= (this->m * racional.n);
+}
+
---
+++ proyecto2/racional.hpp
@@ -0,0 +1,43 @@
+#pragma once
+#include <math.h>
+
+//get MCD
+int getMCD(int numero1, int numero2);
+
+class Racional
+{
+private:
+	int n, m; //m!=0 y n, m tales que: m.c.d.(n,m)==1.
+	//miscelaneas
+	Racional simplificar();
+public:
+	// Constructores, destructor
+	Racional(int, int);
+	~Racional();
+	//// Proyecciones
+	inline int num() { return this->n; } //numerador
+	inline int den() { return this->m; } //denominador
+	//// Asignacion
+	Racional operator=(Racional);
+	//// Operadores aritmeticos
+	////unarios
+	Racional operator+();
+	Racional operator-();
+	////binarios
+	Racional operator+(Racional); // adicion
+	Racional operator-(Racional); // diferencia
+	Racional operator*(Racional); // producto
+	Racional operator/(Racional); // cociente (con dividendo diferente de cero)
+	////incrementales
+	void operator-=(Racional);
+	void operator+=(Racional);
+	void operator*=(Racional);
+	void operator/=(Racional);
+	//// Operadores "booleanos"
+	bool operator== (Racional); // igualdad
+	bool operator!= (Racional); // desigualdad
+	bool operator> (Racional); // mayor estricto
+	bool operator>= (Racional); // mayor igual
+	bool operator< (Racional); // menor estricto
+	bool operator<= (Racional); // menor igual
+};
